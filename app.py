99983402098a1ae618900ed36b35ff4fee98c0de
maelmoignet-pixel <mael.moignet@gmail.com>
2026-09-16 18:16:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,5 @@
 import streamlit as st
 
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["app_password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input("Mot de passe", type="password", on_change=password_entered, key="password")
-        st.error("Mot de passe incorrect")
-        return False
-    else:
-        return True
-
-if check_password():
-
 st.set_page_config(page_title="Mes cours de maths - Terminale", page_icon="📚", layout="wide")
 
 # --- Empêche les navigateurs (Chrome, etc.) de proposer/forcer une
